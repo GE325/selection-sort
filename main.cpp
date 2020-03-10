d911561cecd235e1599b869a0f21ad8c723b5eb7
GE325 <60921786+GE325@users.noreply.github.com>
2020-03-10 20:36:45 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
 
 using namespace std;
-void swapping(int a, int b)
-{
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-void display(int *array, int size)
-{
-    for(int i=0; i<size; i++)
-        cout << array[i] << " ";
-    cout << endl;
-}
-
-void selectionSort(int *array, int size)
-{
-    int i ,j , imin;
-    for(i = 0; i<size-1; i++)
-    {
-        imin = i; //get index of minimum data
-        for(j = i+1; j<size; j++)
-            if(array[j] < array[imin])
-            imin = j;
-        //placing in correct position
-        swap(array[i], array[imin]);
-    }
-}
-
 
 int main()
 {
-    int n;
-    cout<<"Enter the number of elements :";
-    cin >> n;
-    int arr[n]; //create an array with given number of elements
-    cout << "Enter elements:"<<endl;
-
-    for(int i =0; i<n; i++)
+    int array[5];
+    cout<<" Enter 5 numbers randomly: "<< endl ;
+    for (int i=0; i<5; i++)
     {
-        cin >> arr[i];
+        cin >> array[i];
     }
+    cout << endl;
+    cout << "Input array is : " << endl;
+    for( int j=0; j<5; j++)
+    {
+        cout <<"\t\t\tValue at " << j << "Index:  " << array[j] << endl;
+    }
+    cout << endl;
+    //Bubble sort starts here
+    int temp;
+    for (int i2 =0; i2<=4; i2++)
+    {
+        for( int j =0; j<4; j++)
+        {
+            //Swapping element in if statement
+            if (array[j] > array[j+1] )
+            {
+                temp=array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    //Displaying sorted array
+    cout<<"Sorted array is : " <<endl;
+    for(int i =0; i<5; i++)
+    cout << "\t\t\t Value at " <<i << "index :  "<<array[i] << endl;
 
-    cout << "Array before Sorting: ";
-    display(arr,n);
-    selectionSort(arr,n);
-    cout<<"Array after sorting: ";
-    display(arr,n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
